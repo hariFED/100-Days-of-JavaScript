fc8a6fe99d31a6ad881a88169ed839e961c0da96
Hariharan <109363771+hariFED@users.noreply.github.com>
2023-07-28 08:09:02 +0530

    Project-7 Click to Copy
    Creating a copy function in JavaScript

---
+++ --Click-to-copy/script.js
@@ -0,0 +1,22 @@
+//Palindrome Checker
+
+const copyBtn = document.querySelector('button')
+const input = document.querySelector('input')
+
+copyBtn.addEventListener('click', copyText)
+
+
+
+function copyText() {
+    navigator.clipboard.writeText(input.value)
+    copyBtn.innerHTML = "Copied..!"
+    setTimeout(()=>{
+        copyBtn.innerHTML="Copy"
+    },3000)
+}
+
+
+
+
+
+
