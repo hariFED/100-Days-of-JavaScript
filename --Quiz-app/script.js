be395b14c60fd31ccd3b3e5842ae8f6f677e6f40
Hariharan <109363771+hariFED@users.noreply.github.com>
2023-07-30 22:34:22 +0530

    Update

--- --Quiz-app/script.js
+++ --Quiz-app/script.js
@@ -1,50 +1,50 @@
 
-class Quiz{
-    constructor(questions){
-        this.score = 0;
-        this.questions = questions;
-        this.questionIndex = 0;
-    }
+// class Quiz{
+//     constructor(questions){
+//         this.score = 0;
+//         this.questions = questions;
+//         this.questionIndex = 0;
+//     }
 
-    getQuestionIndex(){
-        return this.questions[this.questionIndex]
-    }
+//     getQuestionIndex(){
+//         return this.questions[this.questionIndex]
+//     }
 
-    guess(answer){
-        if(this.getQuestionIndex().isCorrectAnswer(answer)){
-            this.score++
-        }
-        this.questionIndex++;
-    }
-    isEnd(){
-        return this.questionIndex === this.questions.length
-    }
+//     guess(answer){
+//         if(this.getQuestionIndex().isCorrectAnswer(answer)){
+//             this.score++
+//         }
+//         this.questionIndex++;
+//     }
+//     isEnd(){
+//         return this.questionIndex === this.questions.length
+//     }
 
-}
-
-
-
-class Questions{
-    constructor(text, options, answer){
-        this.text = text;
-        this.options =options;
-        this.answer = answer;
-    }
-
-    isCorrectAnswer(options){
-        return this.answer === options;
-    }
-}
+// }
 
 
-function displayQuestions(){
-    if(Quiz.isEnd()){
-        finalScore();
-    }
-    else{
-        let questionElem = document.getElementById('quiz-question')
-        questionElem.innerHTML = Quiz.getQuestionIndex().text;
+
+// class Questions{
+//     constructor(text, options, answer){
+//         this.text = text;
+//         this.options =options;
+//         this.answer = answer;
+//     }
+
+//     isCorrectAnswer(options){
+//         return this.answer === options;
+//     }
+// }
+
+
+// function displayQuestions(){
+//     if(Quiz.isEnd()){
+//         finalScore();
+//     }
+//     else{
+//         let questionElem = document.getElementById('quiz-question')
+//         questionElem.innerHTML = Quiz.getQuestionIndex().text;
 
         
-    }
-}
+//     }
+// }
